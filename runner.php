6f13523a4c9cf5356dbab952c48bd2b8d6e0b0b6
Mike Moore <tigerhollowpoint@gmail.com>
2018-02-18 21:50:05 -0500

    ddd
    dddd

--- runner.php
+++ runner.php
@@ -1,13 +1,15 @@
 <?php
+date_default_timezone_set('UTC');
+ini_set('auto_detect_line_endings',TRUE);
 //AUTOMATION
 chdir(dirname(__FILE__));
 
 //MAKE IT SO IT CAN ONLY BE RUN ON CRON / Swap dev spreadsheet
-$spreadsheet_url = 'dev_spread.csv';
+$spreadsheet_url = 'dev_spread.html';
 if($_GET['dev'] !== 'yes') {
   if( php_sapi_name() !== 'cli' ){http_response_code(404); die();}
   //TY Mother Jones
-  $spreadsheet_url = 'https://docs.google.com/spreadsheet/pub?key=0AswaDV9q95oZdG5fVGJTS25GQXhSTDFpZXE0RHhUdkE&amp;output=csv'
+  $spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1XV4mZi3gYDgwx5PrLwqqHTUlHkwkV-6uy_yeJh3X46o/pub';
 }
 
 
@@ -16,11 +18,56 @@ if(!$spreadsheet) {
   http_response_code(404);
   die();
 }
-$run_info = json_decode(@file_get_contents('run_info.json'));
-//GET FIRST ONE;
+$dom = new DOMDocument;
+$dom->loadHTML($spreadsheet);
 
-//NOTHING NEW
-if($newest['shooting_title'] === $run_info['shooting_title'] || $newest['shooting_location'] === $newest['shooting_location']) {
+$parsed_table = [];
+
+foreach($dom->getElementsbyTagName('tr') as $tr) {
+  if(empty($tr->nodeValue)) {
+    continue;
+  }
+  $item = [];
+  foreach($tr->getElementsbyTagName('td') as $td) {
+    if(!empty($td->nodeValue)) {
+      $item[] = $td->nodeValue;
+    }
+  }
+  $parsed_table[] = $item;
+}
+
+//GET FIRST ONE;
+$first = [];
+foreach($parsed_table[0] as $k => $h) {
+  $first[$h] = $parsed_table[1][$k];
+}
+
+$newest = [];
+$newest['shooting_title'] = trim($first['Case']);
+$location = explode(',',$first['Location']);
+if(count($location) > 1) {
+  $newest['shooting_city'] = trim($location[0]);
+  $newest['shooting_state'] = trim($location[1]);
+} else {
+  $newest['shooting_city'] = trim($location[0]);
+  $newest['shooting_state'] = '';
+}
+$newest['shooting_date'] = strtotime($first['Date']);
+
+
+
+$run_info = json_decode(@file_get_contents('run_info.json'),TRUE);
+
+$comparers = ['shooting_title','shooting_city','shooting_state','shooting_date'];
+$same_count = 0;
+//COMPARE ITEMS
+foreach($comparers as $c) {
+  if($newest[$c] === $run_info[$c]) {
+    $same_count++;
+  }
+}
+//IF 2 or more are the same, it's probably the same
+if($same_count >= 2) {
   http_response_code(404);
   die();
 }
@@ -39,15 +86,15 @@ $settings = array(
 );
 
 //LOAD IN TWEET TEMPLATES
-$templates = json_decode(@file_get_contents('tweet_templates.json'));
+$templates = json_decode(@file_get_contents('tweet_templates.json'),true);
 $template_number = $run_info['last_template']++;
 if($template_number > count($templates) - 1) {
- $template_number = 0; 
+ $template_number = 0;
 }
-//GET JUST THE CITY
-$city = trim(explode(',',$newest['shooting_location'])[0]); 
+
+
 //MAKE THE TWEET
-$tweet_text = str_replace("***SHOOTING LOCATION***",$city,$templates[$template_number]);
+$tweet_text = str_replace("***SHOOTING LOCATION***",$newest['shooting_city'],$templates[$template_number]);
 
 //SEND THE TWEET
 $twitterNew = new TwitterAPIExchange($settings);
@@ -56,9 +103,10 @@ $postfields = array(
 );
 $postedTweet = $twitterNew->buildOauth('https://api.twitter.com/1.1/statuses/update.json', 'POST')->setPostfields($postfields)->performRequest();
 
-if($postedTweet) {
-  $run_info['last_template'] = $template_number;
-  file_put_contents('run_info.json',$run_info);
+
+if($postedTweet['created_at']) {
+  $newest['last_template'] = $template_number;
+  file_put_contents('run_info.json',json_encode($newest));
 }
 
 die();
